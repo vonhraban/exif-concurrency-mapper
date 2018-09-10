43460f54ac26dfda95c52cbe85bc142700023d54
Vlad <hraban@vivec.co.uk>
2018-09-10 22:21:10 +0200

    Serve arbitrary directory in the server isntead of changing cwd

--- http_server/server.py
+++ http_server/server.py
@@ -5,13 +5,29 @@ import os
 PORT = 8001
 
 
+class HTTPHandler(http.server.SimpleHTTPRequestHandler):
+    """This handler uses server.base_path instead of always using os.getcwd()"""
+    def translate_path(self, path):
+        path = http.server.SimpleHTTPRequestHandler.translate_path(self, path)
+        relpath = os.path.relpath(path, os.getcwd())
+        return os.path.join(self.server.base_path, relpath)
+
+
+class HTTPServer(http.server.HTTPServer):
+    """The main server, you pass in base_path which is the path you want to serve requests from"""
+    def __init__(self, base_path, server_address):
+        self.base_path = base_path
+        http.server.HTTPServer.__init__(self, server_address=server_address, RequestHandlerClass=HTTPHandler)
+
+
 def create():
-    web_dir = os.path.join(os.path.dirname(__file__), '../static')
-    os.chdir(web_dir)
-    handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(("", PORT), handler)
+    httpd = HTTPServer('./static', ("", PORT))
     print("serving at port:" + str(PORT))
-    httpd.serve_forever()
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
 
 
 def get_url():
